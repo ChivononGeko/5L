8a666a13f757dc8e873193ced2816d9beab2a8be
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2020-11-25 03:23:28 +0600

    Add files via upload

---
+++ ConsoleApp34/ConsoleApp34/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace l5t15
+
+{
+
+    /*
+
+    * Внутри метода Main() создайте программу, которая считывает 5 чисел с клавиатуры
+
+    * и выводит в консоль наименьшее из них.
+
+    */
+
+    public class Program
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            /* Добавьте свой код ниже */
+            int b = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a<b)
+                {
+                    b = a;
+                }
+            }
+            Console.WriteLine(b);
+
+        }
+
+    }
+
+}
