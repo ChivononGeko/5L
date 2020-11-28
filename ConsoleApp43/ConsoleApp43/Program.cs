334fc36cb82ea449812ac7187d87032d1d9a83a7
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2020-11-29 02:00:26 +0600

    Add files via upload

---
+++ ConsoleApp43/ConsoleApp43/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace l5t11
+
+{
+
+    /*
+
+    * Постарайтесь создать для класса Cat как можно больше конструкторов.
+
+    * Шаблон задачи менять нельзя.
+
+    */
+
+    public class Cat
+
+    {
+
+        int age;
+
+        int weight;
+
+        string color;
+
+        string name;
+
+        /* Добавьте свой код ниже */
+        public Cat()
+        { }
+        public Cat(int age)
+        { }
+        public Cat(int age, int weight)
+        { }
+        public Cat(int age, int weight, string color)
+        { }
+        public Cat(int age, int weight, string color, string name)
+        { }
+
+        public static void Main(string[] args)
+
+        {
+
+        }
+
+    }
+
+}
