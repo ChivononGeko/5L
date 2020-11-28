db6a0141fec4bc0d3aa9e78551f9629698278b9d
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2020-11-29 02:00:01 +0600

    Add files via upload

---
+++ ConsoleApp42/ConsoleApp42/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace l5t9
+
+{
+
+    /*
+
+    * Реализуйте класс Rectangle, который содержит четыре целочисленные переменные - координаты Х вершин прямоугольника.
+
+    * Создайте 5 конструкторов для класса Rectangle.
+
+    */
+
+    public class Rectangle
+
+    {
+        /* Добавьте свой код ниже */
+        int a;
+        int b;
+        int c;
+        int d;
+        public Rectangle()
+        { }
+        public Rectangle(int a)
+        { }
+        public Rectangle(int a, int b)
+        { }
+        public Rectangle(int a, int b, int c)
+        { }
+        public Rectangle(int a, int b, int c, int d)
+        { }
+
+        public static void Main(string[] args)
+
+        {
+
+        }
+
+    }
+
+}
