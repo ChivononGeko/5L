23f4dc98e4de68127f13af5e9c441cda1589d25f
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2020-11-29 01:59:21 +0600

    Add files via upload

---
+++ ConsoleApp41/ConsoleApp41/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace l5t6
+
+{
+
+    /*
+
+    * Создайте новый класс Cat в том же файле и том же namespace, что и класс Program.
+
+    * В классе Cat создайте 6 переменных, имена, типы переменных и модификатор доступа переменных придумайте самостоятельно.
+
+    * Создайте 7 различных конструкторов для класса Cat.
+
+    */
+
+    public class Program
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            /* Добавьте свой код ниже */
+
+        }
+
+    }
+    public class Cat
+    {
+        int a;
+        string b;
+        int c;
+        int d;
+        string e;
+        string f;
+
+        public Cat()
+        {  }
+        public Cat (int a)
+        { }
+        public Cat(int a, string b)
+        { }
+        public Cat(int a, string b, int c)
+        { }
+        public Cat(int a, string b, int c, int d)
+        { }
+        public Cat(int a, string b, int c, int d, string e)
+        { }
+        public Cat(int a, string b, int c, int d, string e, string f)
+        { }
+
+    }
+
+}
