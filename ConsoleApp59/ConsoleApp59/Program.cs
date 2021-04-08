16e5728092ef49983e32eb71b6609c50d984ff55
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-04-08 20:35:18 +0600

    Add files via upload

---
+++ ConsoleApp59/ConsoleApp59/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace l5t29
+{
+    /*
+    * В классе Program добавьте публичное статическое поле deleteCount, которое должно увеличиваться каждый раз при уничтожении
+    * экземпляра класса. Добавьте соответствующую логику деструктору класса Program
+    */
+    public class Program
+    {
+        /* Добавьте свой код ниже */
+        public static int deleteCount;
+        public static void Main(string[] args)
+        {
+        }
+       ~Program()
+        {
+            Program.deleteCount++;
+        }
+    }
+}
