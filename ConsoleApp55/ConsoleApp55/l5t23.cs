b43061707552e8b654dd7610c03657ae0596f7ff
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-04-08 20:26:07 +0600

    Add files via upload

---
+++ ConsoleApp55/ConsoleApp55/l5t23.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace l5t23
+
+{
+
+    /*
+
+    * Для всех приватных полей класса Walkman создайте и реализуйте соответствующие сеттеры и геттеры.
+
+    * Для сеттера поля soundVolume необходимо:
+
+    * 1) Устанавливать значение 0, если передано отрицательное значение.
+
+    * 2) Устанавливать значение 100, если передано значение большее 100.
+
+    * В сеттере поля currentSong необходимо устанавливать значение полю isOn - false, если текущее значение поля null или пустая строка.
+
+    * Шаблон задачи изменять нельзя.
+
+    */
+
+    public class Walkman
+
+    {
+
+        private int soundVolume = 50;
+
+        private string currentSong = "Oooo...you touch my tralala";
+
+        public bool isOn = true;
+
+        public static void Main(string[] args)
+
+        {
+
+        }
+
+        /* Добавьте свой код ниже */
+        public int Volume
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    soundVolume = 0;
+                }
+                else if (value > 100)
+                {
+                    soundVolume = 100;
+                }
+                else
+                {
+                    soundVolume = value;
+                }
+            }
+        }
+        public string currentsong
+        {
+            set
+            {
+                if (value == null)
+                {
+                    isOn = false;
+                }
+                else if (value == "")
+                {
+                    isOn = false;
+                }
+                else
+                {
+                    isOn = true;
+                }
+            }
+        }
+
+    }
+
+}
