11a88a3a1ba4892f716ec1aee665644e14f02bf1
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-04-10 00:43:46 +0600

    Add files via upload

---
+++ ConsoleApp64/ConsoleApp64/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace l5t34
+{
+    /*
+    * Создайте статический конструктор для класса Program, который должен выводить в консоль
+    * число 8 в 10 степени, с использованием метода Pow() статического класса Math.
+    */
+    public class Program
+    {
+        /* Добавьте свой код ниже */
+        static Program()
+        {
+            int a = Convert.ToInt32(Math.Pow(8, 10));
+        }
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
